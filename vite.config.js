43da200081b8571b01b37b9e49df2aa254f2ce8a
rashmiranjan.rout <rashmiranjan.rout@arvindinternet.com>
2024-04-27 16:41:24 +0530

    config change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['react-icons/all']
+    }
+  }
+});
+
